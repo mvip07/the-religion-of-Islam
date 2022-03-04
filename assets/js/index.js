b2bf15f15c45dcb50bc8048c53940e47d5ae9f9d
mirabzal-07 <mirabzalozodov07@gmail.com>
2022-03-04 15:21:54 +0500

    assets qo'shildi assets ichidagi ma' lumot to'g'rlandi mobilennida sahifada sahifaga otishda muamoni tuzatildi contact faildida inputadan kirgan ma' lumotni yuboorish qo'shildi
    Signed-off-by: mirabzal-07 <mirabzalozodov07@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,50 +3,46 @@ let geo = navigator.geolocation
 let apiBase = "https://api.openweathermap.org/data/2.5"
 let apiKey = "de3ae2261305416619b29ff9f5edc781"
 const Product = document.querySelector(".product__catagory")
-if (geo) {
-    geo.getCurrentPosition(function (position) {
-        const {longitude, latitude} = position.coords
+geo.getCurrentPosition(function (position) {
+    const {longitude, latitude} = position.coords
 
-        // https://api.openweathermap.org/data/2.5/onecall?lat=47.667&lon=69.3850&exclude=current&appid=de3ae2261305416619b29ff9f5edc781&units=metric
-        fetch(`${apiBase}/onecall?lat=${latitude}&lon=${longitude}&exclude=current&appid=${apiKey}&units=metric`)
-            .then(data => data.json())
-            .then((weather) => {
+    fetch(`${apiBase}/onecall?lat=${latitude}&lon=${longitude}&exclude=current&appid=${apiKey}&units=metric`)
+        .then(data => data.json())
+        .then((weather) => {
 
-                let weatherDaily = weather.daily
+            let weatherDaily = weather.daily
 
-                for (let i of weatherDaily) {
-                    var dayname = new Date(i.dt * 1000).toLocaleDateString({weekday: ""});
+            for (let i of weatherDaily) {
+                var dayname = new Date(i.dt * 1000).toLocaleDateString({weekday: ""});
 
-                    const productCatagory = document.createElement("div")
-                    productCatagory.classList.add("product__catagory--single")
+                const productCatagory = document.createElement("div")
+                productCatagory.classList.add("product__catagory--single")
 
-                    const ProductContent = document.createElement("div")
-                    //ProductContent.classList.add("product__content product__content--catagory")
-                    ProductContent.innerHTML = `
-                    <p class="product__link">Sana: ${dayname}</p>
-                    <p class="product__link">Max: ${i.temp.max}</p>
-                    <span class="product__items--text">Min: ${i.temp.min}</span>
-                `
+                const ProductContent = document.createElement("div")
+                //ProductContent.classList.add("product__content product__content--catagory")
+                ProductContent.innerHTML = `
+                <p class="product__link">Sana: ${dayname}</p>
+                <p class="product__link">Max: ${i.temp.max}</p>
+                <span class="product__items--text">Min: ${i.temp.min}</span>
+            `
 
-                    const ProductImgBox = document.createElement("div")
-                    //ProductImgBox.classList.add("product__img-box product__img-box--catagory")
-                    ProductImgBox.innerHTML = `
-                    <p class="product__img--link">
-                        <img class="product__img img-fluid" src="http://openweathermap.org/img/wn/${i.weather[0].icon}.png" alt="">
-                        <p class="product__link">${i.weather[0].main}</p>
-                    </p>
-                `
+                const ProductImgBox = document.createElement("div")
+                //ProductImgBox.classList.add("product__img-box product__img-box--catagory")
+                ProductImgBox.innerHTML = `
+                <p class="product__img--link">
+                    <img class="product__img img-fluid" src="http://openweathermap.org/img/wn/${i.weather[0].icon}.png" alt="">
+                    <p class="product__link">${i.weather[0].main}</p>
+                </p>
+            `
 
-                    productCatagory.append(ProductContent, ProductImgBox)
-                    Product.append(productCatagory)
+                productCatagory.append(ProductContent, ProductImgBox)
+                Product.append(productCatagory)
 
-                }
-            })
-    })
-}
+            }
+        })
+})
 
 /* BU qismda namoz vaqtlarini ko'rsati beruvchi code qismi*/
-
 const ProductBox = document.querySelector("#product-default-slider")
 let today = new Date()
 let day = `${today.getDate() < 10 ? "0" : ""}${today.getDate()}`
@@ -54,92 +50,81 @@ let month = `${(today.getMonth() + 1) < 10 ? "0" : " "}${today.getMonth() + 1}`
 let year = `${today.getFullYear()}`
 let dd_aa_yyyy = `${year}-${month}-${day}`
 
-if (geo) {
-    geo.getCurrentPosition(function (position) {
-        const {longitude, latitude} = position.coords
+geo.getCurrentPosition(function (position) {
+    const {longitude, latitude} = position.coords
 
-        fetch(`https://api.pray.zone/v2/times/dates.json?longitude=${longitude}&latitude=${latitude}&elevation=333&start=${dd_aa_yyyy}&end=2022-03-06`)
-            .then((response) => response.json())
-            .then(prayer => {
+    fetch(`https://api.pray.zone/v2/times/dates.json?longitude=${longitude}&latitude=${latitude}&elevation=333&start=${dd_aa_yyyy}&end=2022-03-11`)
+        .then((response) => response.json())
+        .then(prayer => {
 
-                let prayerData = prayer.results.datetime
+            let prayerData = prayer.results.datetime
 
-                for (let prayerDatas of prayerData) {
-                    const productContent = document.createElement("div")
-                    productContent.classList.add("product__box"), productContent.classList.add("product__default--single")
-                    productContent.innerHTML = `
-                <div class="product__content">
-                    <p class="product__link">${prayerDatas.date.hijri}</p>
-                    <div class="product__price m-t-5">
-                        <span class="product__price">${prayerDatas.date.gregorian}</span>
-                    </div>
+            for (let prayerDatas of prayerData) {
+                const productContent = document.createElement("div")
+                productContent.classList.add("product__box"), productContent.classList.add("product__default--single")
+                productContent.innerHTML = `
+            <div class="product__content">
+                <p class="product__link">${prayerDatas.date.hijri}</p>
+                <div class="product__price m-t-5">
+                    <span class="product__price">${prayerDatas.date.gregorian}</span>
+                </div>
 
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                        <p class="product__link">Bomdod:</p>
-                        <p class="product__link">${prayerDatas.times.Fajr}</p>
-                    </div>
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                    <p class="product__link">Bomdod:</p>
+                    <p class="product__link">${prayerDatas.times.Fajr}</p>
+                </div>
 
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                        <p class="product__link">Quyosh chiqishi: </p>
-                        <p class="product__link">${prayerDatas.times.Sunrise}</p>
-                    </div>
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                    <p class="product__link">Quyosh chiqishi: </p>
+                    <p class="product__link">${prayerDatas.times.Sunrise}</p>
+                </div>
 
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                        <p class="product__link">Peshin:</p>
-                        <p class="product__link">${prayerDatas.times.Dhuhr}</p>
-                    </div>
-
-
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                        <p class="product__link">Asr:</p>
-                        <p class="product__link">${prayerDatas.times.Asr}</p>
-                    </div>
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                    <p class="product__link">Peshin:</p>
+                    <p class="product__link">${prayerDatas.times.Dhuhr}</p>
+                </div>
 
 
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                        <p class="product__link">Shom</p>
-                        <p class="product__link">${prayerDatas.times.Maghrib}</p>
-                    </div>
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                    <p class="product__link">Asr:</p>
+                    <p class="product__link">${prayerDatas.times.Asr}</p>
+                </div>
 
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                        <p class="product__link">Quyosh botishi:</p>
-                        <p class="product__link">${prayerDatas.times.Sunset}</p>
-                    </div>
 
-                    <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
-                    
-                        <p class="product__link">Xufton:</p>
-                        <p class="product__link">${prayerDatas.times.Isha}</p>
-                    </div>
-                </div>                        
-                `
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                    <p class="product__link">Shom</p>
+                    <p class="product__link">${prayerDatas.times.Maghrib}</p>
+                </div>
 
-                    ProductBox.append(productContent)
-                }
-            })
-    })
-}
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                    <p class="product__link">Quyosh botishi:</p>
+                    <p class="product__link">${prayerDatas.times.Sunset}</p>
+                </div>
+
+                <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
+                
+                    <p class="product__link">Xufton:</p>
+                    <p class="product__link">${prayerDatas.times.Isha}</p>
+                </div>
+            </div>                        
+            `
+
+                ProductBox.append(productContent)
+            }
+        })
+})
 
 let map = document.querySelector("#map")
-if (geo) {
-    geo.getCurrentPosition(function (position) {
+geo.getCurrentPosition(function (position) {
 
-        const {latitude, longitude} = position.coords
-        map = L.map('map').setView([latitude, longitude], 15);
+    const {latitude, longitude} = position.coords
+    map = L.map('map').setView([latitude, longitude], 15);
 
-        console.log(latitude, longitude)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        L.marker([41.3264751, 69.2277542]).addTo(map)
-            .bindPopup('Our headquarters')
-            .openPopup();
-
-
-        L.marker({latitude, longitude}).addTo(map)
-            .bindPopup('You are here')
-            .openPopup();
-    })
-}
+    L.marker([41.3264751, 69.2277542]).addTo(map)
+        .bindPopup('Our headquarters')
+        .openPopup();
+})
