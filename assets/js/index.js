c81a421d63e4815f8d8bd392520599f4d0269d14
mirabzal-07 <mirabzalozodov07@gmail.com>
2022-08-23 17:37:23 +0500

    prayer new api was conducted

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,68 +45,66 @@ geo.getCurrentPosition(function (position) {
 /* BU qismda namoz vaqtlarini ko'rsati beruvchi code qismi*/
 const ProductBox = document.querySelector("#product-default-slider");
 let today = new Date();
-let day = `${today.getDate() < 10 ? "0" : ""}${today.getDate()}`;
 let month = `${today.getMonth() + 1 < 10 ? "0" : " "}${today.getMonth() + 1}`;
 let year = `${today.getFullYear()}`;
-let dd_aa_yyyy = `${year}-${month}-${day}`;
 
 geo.getCurrentPosition(function (position) {
   const { longitude, latitude } = position.coords;
 
   fetch(
-    `https://api.pray.zone/v2/times/dates.json?longitude=${longitude}&latitude=${latitude}&elevation=333&start=${dd_aa_yyyy}&end=2022-05-31`
+    `http://api.aladhan.com/v1/calendarByAddress?address=%20${latitude},%20${longitude}&method=2&month=${month}&year=${year}`
   )
     .then((response) => response.json())
     .then((prayer) => {
-      let prayerData = prayer.results.datetime;
+      let prayerDatas = prayer.data;
 
-      for (let prayerDatas of prayerData) {
+      for (let prayerData of prayerDatas) {
         const productContent = document.createElement("div");
         productContent.classList.add("product__box"),
           productContent.classList.add("product__default--single");
         productContent.innerHTML = `
             <div class="product__content">
-                <p class="product__link">${prayerDatas.date.hijri}</p>
+                <p class="product__link">${prayerData.date.hijri.date}</p>
                 <div class="product__price m-t-5">
-                    <span class="product__price">${prayerDatas.date.gregorian}</span>
+                    <span class="product__price">${prayerData.date.readable}</span>
                 </div>
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                     <p class="product__link">Bomdod:</p>
-                    <p class="product__link">${prayerDatas.times.Fajr}</p>
+                    <p class="product__link">${prayerData.timings.Fajr}</p>
                 </div>
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                     <p class="product__link">Quyosh chiqishi: </p>
-                    <p class="product__link">${prayerDatas.times.Sunrise}</p>
+                    <p class="product__link">${prayerData.timings.Sunrise}</p>
                 </div>
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                     <p class="product__link">Peshin:</p>
-                    <p class="product__link">${prayerDatas.times.Dhuhr}</p>
+                    <p class="product__link">${prayerData.timings.Dhuhr}</p>
                 </div>
 
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                     <p class="product__link">Asr:</p>
-                    <p class="product__link">${prayerDatas.times.Asr}</p>
+                    <p class="product__link">${prayerData.timings.Asr}</p>
                 </div>
 
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                     <p class="product__link">Shom</p>
-                    <p class="product__link">${prayerDatas.times.Maghrib}</p>
+                    <p class="product__link">${prayerData.timings.Maghrib}</p>
                 </div>
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                     <p class="product__link">Quyosh botishi:</p>
-                    <p class="product__link">${prayerDatas.times.Sunset}</p>
+                    <p class="product__link">${prayerData.timings.Sunset}</p>
                 </div>
 
                 <div class="product__times--time d-flex align-items-center justify-content-between p-1 mx-3">
                 
                     <p class="product__link">Xufton:</p>
-                    <p class="product__link">${prayerDatas.times.Isha}</p>
+                    <p class="product__link">${prayerData.timings.Isha}</p>
                 </div>
             </div>                        
             `;
